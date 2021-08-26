e7a290defedf1967ef6264d7ade6ce4c88b3d0fc
jeff-romero <jeffreybrianr@yahoo.com>
2021-08-26 03:51:10 -0700

    Changed program appearance

--- Python/counter/counter.py
+++ Python/counter/counter.py
@@ -1,8 +1,9 @@
 # Jeffrey Romero
 # GitHub: jeff-romero
 
+from os import error
 import tkinter as tk
-from tkinter import ttk
+from tkinter.ttk import Combobox, Separator
 import json
 from os.path import exists
 import genstring
@@ -11,62 +12,70 @@ genstring.declare_usable_characters('ALPHA', 'NUM')
 SCREEN_WIDTH = 720
 SCREEN_HEIGHT = 240
 screen_size = str(SCREEN_WIDTH) + "x" + str(SCREEN_HEIGHT)
-# canvas = tk.Canvas(root, height=SCREEN_HEIGHT, width=SCREEN_WIDTH)
 root = tk.Tk()
 root.title('Counter Tool')
 root.geometry(screen_size)
 
 
-main_buttons_canvas = tk.Canvas(root, bd=0, bg='green', height=SCREEN_HEIGHT, width=360)
-main_buttons_canvas.pack(side=tk.LEFT, fill='both', expand=True)
-recipe_canvas = tk.Canvas(root, bd=0, bg='red', height=SCREEN_HEIGHT, width=360)
-recipe_canvas.pack(side=tk.RIGHT, fill='both', expand=True)
+main_buttons_frame = tk.Frame(root, bg='grey')
+main_buttons_frame.pack(side=tk.LEFT, fill='both', expand=True)
 
+recipes_frame = tk.Frame(root, bg='grey')
+recipes_frame.pack(side=tk.RIGHT, fill='both', expand=True)
 
 # GLOBALS
-current_count = 0
-labels = [] # Will hold recipes
+__current_count = 0
+__labels = [] # Will hold recipes
+__ADD_COLOR = '#81d41a'
+__SUB_COLOR = '#ff0000'
+__MAX_VAL = 100 # Maximum value to add/subtract
 
 
 def inc(label=None, num_to_inc=1):
-    global current_count
-    current_count += num_to_inc
-    display = str(current_count)
-    label.config(text=display)
+    if num_to_inc <= __MAX_VAL:
+        global __current_count
+        __current_count += num_to_inc
+        display = str(__current_count)
+        label.config(text=display)
+    else:
+        print('Chosen value too large! (Required: value <= ' + str(__MAX_VAL) + ')')
 
 
 def dec(label=None, num_to_dec=1):
-    global current_count
-    current_count -= num_to_dec
-    display = str(current_count)
-    label.config(text=display)
+    if num_to_dec <= __MAX_VAL:
+        global __current_count
+        __current_count -= num_to_dec
+        display = str(__current_count)
+        label.config(text=display)
+    else:
+        print('Chosen value too large! (Required: value <= ' + str(__MAX_VAL) + ')')
 
 
 def reset(label=None):
-    global current_count
-    current_count = 0
-    display = str(current_count)
+    global __current_count
+    __current_count = 0
+    display = str(__current_count)
     label.config(text=display)
 
 
 def create_data_file(data_file=None):
-    file_contents = '{\"current_count\" : ' + str(current_count) + '}'
+    file_contents = '{\"current_count\" : ' + str(__current_count) + '}'
     f = open(data_file, 'w+')
     f.write(file_contents)
     f.close()
 
 
 def load_data_file(data_file=None):
-    global current_count, data
+    global __current_count, data
     f = open(data_file, 'r')
     data = json.load(f)
-    current_count = data['current_count']
+    __current_count = data['current_count']
     f.close()
 
 
 def program_exit():
     global data
-    data['current_count'] = current_count
+    data['current_count'] = __current_count
 
     f = open(data_file, 'w+')
     f.write(json.dumps(data))
@@ -75,44 +84,26 @@ def program_exit():
     exit(0)
 
 
-def shift_array_after_del(arr=[]):
-    if len(arr) > 1:
-        for widget in arr:
-            if (widget == None):
-                print('asd')
-            print(widget)
-        print()
-
-
 def del_button(label_to_del=None):
     if label_to_del:
-        global labels
+        global __labels
         label_to_del.destroy()
-        # del label_to_del
-        labels.remove(label_to_del)
-        shift_array_after_del(labels)
+        __labels.remove(label_to_del)
 
 
 def add_new_recipe():
-    global labels
-    # for i in labels:
-    #     print(i)
-    # print()
-    new_label = tk.Frame(recipe_canvas)
-    new_label.grid(row=len(labels) + 1, column=0)
-    labels.append(new_label)
+    global __labels
+    new_label_frame = tk.Frame(recipes_frame)
+    new_label_frame.pack(fill='x')
+    __labels.append(new_label_frame)
 
-    last_label_in_list = labels[len(labels) - 1]
-    print('last label: ' + str(last_label_in_list))
+    new_label = __labels[len(__labels) - 1]
 
-    labeltext = tk.StringVar()
-    recipe = tk.Label(last_label_in_list, textvariable=labeltext, anchor=tk.W)
-    labeltext.set("New recipe " + genstring.generate_string().ljust(10, ' '))
-    # labeltext = "New recipe " + genstring.generate_string()
-    recipe.grid(row=0, column=0)
+    recipe = tk.Label(new_label, text="New recipe " + genstring.generate_string())
+    recipe.pack(side=tk.LEFT, fill='y', expand=True)
 
-    delete_recipe = tk.Button(last_label_in_list, text='X', bg='red', fg='black', command=lambda:del_button(last_label_in_list), anchor=tk.E)
-    delete_recipe.grid(row=0, column=1)
+    delete_recipe = tk.Button(new_label, text='X', bg='red', fg='black', command=lambda:del_button(new_label))
+    delete_recipe.pack(side=tk.RIGHT, fill='y')
 
 
 data_file = './userdata.json'
@@ -123,59 +114,59 @@ if not exists(data_file):
 load_data_file(data_file)
 
 
-count_label = tk.Label(main_buttons_canvas, text=current_count, height=5, width=10, bg='black', fg='white', justify=tk.CENTER)
-count_label.grid(row=0, pady=(0,20), columnspan=6)
+count_label = tk.Label(main_buttons_frame, text=__current_count, height=3, bg='black', fg='white', justify=tk.CENTER, font=75)
+count_label.pack(fill='x', expand=True, pady=(0,5))
 
 #
 # ADD
 #
-inc_1 = tk.Button(main_buttons_canvas, text='+1', command=lambda:inc(count_label, 1), width=5, bg='black', fg='white')
-inc_1.grid(row=1, column=0, padx=5, pady=(5,2)) # Must be on its own line, can't be compounded with the one above
+add_frame = tk.Frame(main_buttons_frame)
+add_frame.pack(fill='x', expand=True, pady=(0, 5))
 
-inc_2 = tk.Button(main_buttons_canvas, text='+2', command=lambda:inc(count_label, 2), width=5, bg='black', fg='white')
-inc_2.grid(row=1, column=1, padx=5, pady=(5,2))
+__number_options = [
+    1,
+    2,
+    5,
+    10,
+    15,
+    20
+]
 
-inc_5 = tk.Button(main_buttons_canvas, text='+5', command=lambda:inc(count_label, 5), width=5, bg='black', fg='white')
-inc_5.grid(row=1, column=2, padx=5, pady=(5,2))
+add_drop_down_menu = Combobox(add_frame, values=__number_options)
+add_drop_down_menu.set("Select number")
+add_selected_value = tk.Button(add_frame, text='Add', command=lambda:inc(count_label, int(add_drop_down_menu.get())), bg=__ADD_COLOR, fg='black')
 
-inc_10 = tk.Button(main_buttons_canvas, text='+10', command=lambda:inc(count_label, 10), width=5, bg='black', fg='white')
-inc_10.grid(row=1, column=3, padx=5, pady=(5,2))
+add_entry = tk.Entry(add_frame, width=10)
+add_entered_value = tk.Button(add_frame, text='Add', command=lambda:inc(count_label, int(add_entry.get())), bg=__ADD_COLOR, fg='black')
 
-inc_15 = tk.Button(main_buttons_canvas, text='+15', command=lambda:inc(count_label, 15), width=5, bg='black', fg='white')
-inc_15.grid(row=1, column=4, padx=5, pady=(5,2))
+add_entry.pack(side=tk.LEFT)
+add_entered_value.pack(side=tk.LEFT)
 
-inc_20 = tk.Button(main_buttons_canvas, text='+20', command=lambda:inc(count_label, 20), width=5, bg='black', fg='white')
-inc_20.grid(row=1, column=5, padx=5, pady=(5,2))
+add_selected_value.pack(side=tk.RIGHT)
+add_drop_down_menu.pack(side=tk.RIGHT)
+
 
 #
 # SUBTRACT
 #
-dec_1 = tk.Button(main_buttons_canvas, text='-1', command=lambda:dec(count_label, 1), width=5, bg='black', fg='white')
-dec_1.grid(row=2, column=0, padx=5, pady=(2,5))
+subtract_frame = tk.Frame(main_buttons_frame)
+subtract_frame.pack(fill='x', expand=True, pady=(5, 0))
 
-dec_2 = tk.Button(main_buttons_canvas, text='-2', command=lambda:dec(count_label, 2), width=5, bg='black', fg='white')
-dec_2.grid(row=2, column=1, padx=5, pady=(2,5))
+sub_drop_down_menu = Combobox(add_frame, values=__number_options)
+sub_drop_down_menu.set("Select number")
+sub_selected_value = tk.Button(add_frame, text='Sub', command=lambda:dec(count_label, int(sub_drop_down_menu.get())), bg=__SUB_COLOR, fg='black')
 
-dec_5 = tk.Button(main_buttons_canvas, text='-5', command=lambda:dec(count_label, 5), width=5, bg='black', fg='white')
-dec_5.grid(row=2, column=2, padx=5, pady=(2,5))
+sub_entry = tk.Entry(add_frame, width=10)
+sub_entered_value = tk.Button(add_frame, text='Sub', command=lambda:dec(count_label, int(sub_entry.get())), bg=__SUB_COLOR, fg='black')
 
-dec_10 = tk.Button(main_buttons_canvas, text='-10', command=lambda:dec(count_label, 10), width=5, bg='black', fg='white')
-dec_10.grid(row=2, column=3, padx=5, pady=(2,5))
+sub_entry.pack(side=tk.LEFT)
+sub_entered_value.pack(side=tk.LEFT)
 
-dec_15 = tk.Button(main_buttons_canvas, text='-15', command=lambda:dec(count_label, 15), width=5, bg='black', fg='white')
-dec_15.grid(row=2, column=4, padx=5, pady=(2,5))
+sub_selected_value.pack(side=tk.RIGHT)
+sub_drop_down_menu.pack(side=tk.RIGHT)
 
-dec_20 = tk.Button(main_buttons_canvas, text='-20', command=lambda:dec(count_label, 20), width=5, bg='black', fg='white')
-dec_20.grid(row=2, column=5, padx=5, pady=(2,5))
-
-reset_button = tk.Button(main_buttons_canvas, text='RESET', command=lambda:reset(count_label), bg='red', fg='black')
-reset_button.grid(row=2, pady=(60,0), columnspan=6)
-
-# separator = ttk.Separator(recipe_canvas, orient='horizontal')
-# separator.place(relx=0, rely=0.5, relwidth=1, relheight=0.2)
-
-add_new_recipe_button = tk.Button(recipe_canvas, text='Add new recipe', command=add_new_recipe)
-add_new_recipe_button.grid(row=0, column=0)
+add_new_recipe_button = tk.Button(recipes_frame, text='Add new recipe', command=add_new_recipe)
+add_new_recipe_button.pack(side=tk.TOP)
 
 # root.minsize(height=SCREEN_HEIGHT, width=SCREEN_WIDTH)
 root.protocol('WM_DELETE_WINDOW', program_exit)
--- Python/counter/genstring.py
+++ Python/counter/genstring.py
@@ -25,22 +25,3 @@ def generate_string(length=LEN_DEFAULT):
     for i in range(length):
         generated_string += __usable_chars[randint(0, len(__usable_chars) - 1)]
     return generated_string
-
-# class Genstr():
-#     def __init__(self, length=7, ):
-#         self.length = length
-#         self.usable_chars = ''
-    
-    # def declare_usable_characters(self, *char_sets):
-    #     global ALPHA, NUM, SPECIAL
-    #     for char_set in char_sets:
-    #         if char_set == 'ALPHA':
-    #             self.usable_chars += ALPHA
-    #         elif char_set == 'NUM':
-    #             self.usable_chars += NUM
-    #         elif char_set == 'SPECIAL':
-    #             self.usable_chars += SPECIAL
-
-
-
-
